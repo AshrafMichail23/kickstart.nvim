306cf3da3880fb71d9176e2593c6164c120f3cdf
Ashraf Michail <user@noemail.com>
2023-10-09 03:50:47 +0000

    Simplify hop config

--- init.lua
+++ init.lua
@@ -448,4 +448,4 @@ vim.keymap.set('', 'f', function() hop.hint_char1({ direction = directions.AFTER
 vim.keymap.set('', 'F', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false }) end, {remap=true})
 vim.keymap.set('', 't', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 }) end, {remap=true})
 vim.keymap.set('', 'T', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 }) end, {remap=true})
-vim.keymap.set('', 's', function() hop.hint_char1({ direction = nil, current_line_only = false, hint_offset = 1 }) end, {remap=true})
+vim.keymap.set('', 's', function() hop.hint_char1({ current_line_only = false }) end, {remap=true})
