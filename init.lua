b298e3240a2ec0da7edefd59815308d8790f2747
Ashraf Michail <user@noemail.com>
2023-10-09 02:19:57 +0000

    Add Hop

--- init.lua
+++ init.lua
@@ -132,6 +132,12 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  -- Hop
+  {
+    'smoka7/hop.nvim',
+    version = "*",
+    opts = {},
+  },
 }, {})
 
 -- [[ Setting options ]]
@@ -450,3 +456,11 @@ cmp.setup {
   },
 }
 
+-- Configure Hop
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', 'f', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false }) end, {remap=true})
+vim.keymap.set('', 'F', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false }) end, {remap=true})
+vim.keymap.set('', 't', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 }) end, {remap=true})
+vim.keymap.set('', 'T', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 }) end, {remap=true})
+vim.keymap.set('', 's', function() hop.hint_char1({ current_line_only = false, hint_offset = 1 }) end, {remap=true})
